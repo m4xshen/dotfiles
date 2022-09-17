ec93fd1ce98cbd675377668a68363257b2b18d1a
m4xshen <m4xshen@gmail.com>
2022-09-17 09:41:54 +0800

    Update

--- uninstall.sh
+++ uninstall.sh
@@ -9,6 +9,7 @@ stow -t ~/.config -D nvim
 stow -t ~/.config -D polybar
 stow -t ~/.config -D picom
 stow -t ~/.config -D rofi
+stow -t ~/.config -D zathura
 stow -t ~ -D xmonad
 stow -t ~ -D xorg
 stow -t ~ -D bash

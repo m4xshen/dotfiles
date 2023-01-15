4cb2609539632bd5433c91c272df7642a2a43688
m4xshen <m4xshen@gmail.com>
2023-01-15 15:52:15 +0800

    Update

--- install.sh
+++ install.sh
@@ -2,11 +2,10 @@
 
 # sudo pacman -S polybar picom rofi feh fcitx5 unclutter
 
-stow -t ~/.config -S polybar
-stow -t ~/.config -S picom
+stow -t ~/.config -S waybar
+stow -t ~/.config -S hypr
 stow -t ~/.config -S rofi
 stow -t ~/.config -S kitty
 stow -t ~/.config -S zathura
-stow -t ~ -S xmonad
 stow -t ~ -S xorg
 stow -t ~ -S bash

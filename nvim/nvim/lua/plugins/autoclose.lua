045c1729e973e9e64163874422b8be0182f38e5d
m4xshen <m4xshen@gmail.com>
2023-05-11 20:32:56 +0800

    Update

--- nvim/nvim/lua/plugins/autoclose.lua
+++ nvim/nvim/lua/plugins/autoclose.lua
@@ -2,6 +2,9 @@ return {
    {
       "m4xshen/autoclose.nvim",
       opts = {
+         keys = {
+            ["<CR>"] = nil
+         },
          options = {
             disabled_filetypes = { "text", "markdown" },
             disable_when_touch = true,
--- nvim/nvim/lua/plugins/nvim-tree.lua
+++ nvim/nvim/lua/plugins/nvim-tree.lua
@@ -4,8 +4,8 @@ return {
       dependencies = { 'nvim-tree/nvim-web-devicons' },
       opts = {},
       init = function()
-         vim.g.loaded_netrw = 1
-         vim.g.loaded_netrwPlugin = 1
+         -- vim.g.loaded_netrw = 1
+         -- vim.g.loaded_netrwPlugin = 1
       end
    }
 }

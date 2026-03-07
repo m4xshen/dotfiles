7f9512deba438a3a230e064c10fd3af8af62a979
Max Shen <m4xshen@gmail.com>
2026-03-07 15:02:55 -0600

    feat: update nvim

--- nvim/nvim/lua/plugins/git.lua
+++ nvim/nvim/lua/plugins/git.lua
@@ -3,7 +3,22 @@ return {
       "lewis6991/gitsigns.nvim",
       opts = {},
    },
+   -- {
+   --    "tpope/vim-fugitive",
+   -- },
    {
-      "tpope/vim-fugitive",
+      "tanvirtin/vgit.nvim",
+      dependencies = { "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons" },
+      -- Lazy loading on 'VimEnter' event is necessary.
+      event = "VimEnter",
+      config = function()
+         require("vgit").setup({
+            settings = {
+               live_blame = {
+                  enabled = false,
+               },
+            },
+         })
+      end,
    },
 }
